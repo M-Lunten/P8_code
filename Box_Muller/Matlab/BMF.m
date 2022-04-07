4ac8759260503aead3206b49d5cc71ffbb24dc16
M-Lunten <50273518+M-Lunten@users.noreply.github.com>
2022-04-07 16:47:40 +0200

    Fixed point implementation of BM
    It's not yet working

--- Box_Muller/Matlab/BMF.m
+++ Box_Muller/Matlab/BMF.m
@@ -17,25 +17,25 @@ end
 A = i;
 B = 1024 - (i);
 if (B==0)
-    B=1
+    B=1;
 end
 
 if (half2==0) && (negative==0)
-    outputSin = sinAngles(A)
-    outputCos = sinAngles(B)
+    outputSin = sinAngles(A);
+    outputCos = sinAngles(B);
 end
 if (half2==1) && (negative==0)
-    outputSin = sinAngles(B)
-    outputCos = -1 * sinAngles(A)
+    outputSin = sinAngles(B);
+    outputCos = -1 * sinAngles(A);
 end
 if (half2==0) && (negative==1)
-    outputSin = -1 * sinAngles(A)
-    outputCos = -1 * sinAngles(B)
+    outputSin = -1 * sinAngles(A);
+    outputCos = -1 * sinAngles(B);
 end
 if (half2==1) && (negative==1)
-    outputSin = -1 * sinAngles(B)
-    outputCos = sinAngles(A)
+    outputSin = -1 * sinAngles(B);
+    outputCos = sinAngles(A);
 end
-outSin = fi(outputSin,1,16,14)
-outCos = fi(outputCos,1,16,14)
+outSin = fi(outputSin,1,16,14);
+outCos = fi(outputCos,1,16,14);
 
---
+++ Box_Muller/Matlab/BMF_test.m
@@ -0,0 +1,6 @@
+% timing of bmf
+clear all
+
+U = sfi(rand(1), 12, 12);
+
+[s, c] = BMF(U);
