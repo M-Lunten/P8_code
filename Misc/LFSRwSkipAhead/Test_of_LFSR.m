8dd21701af47494a143ee96db9e3eec2c54f8b17
rasm380m <razzer_n@hotmail.com>
2022-04-08 11:32:46 +0200

    LFSR and VHDL generation in 2 seperate functions (Works!)

--- Misc/LFSRwSkipAhead/Test_of_LFSR.m
+++ Misc/LFSRwSkipAhead/Test_of_LFSR.m
@@ -2,4 +2,10 @@ clc
 clear all
 close all
 
-S = LSFR_skip(10,3,2,2);
+Nsamples = 10;
+SR_count = 16;
+Seed = 0;
+Skips = 3;
+
+S = LSFR_skip(Nsamples,SR_count,Seed,Skips);
+LFSR_VHDL_GEN(SR_count,Seed,Skips);
