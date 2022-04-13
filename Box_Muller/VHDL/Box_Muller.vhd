8e1c1159943c526340cf81a310c8db48f555e77a
fivanovic <fivanovic1999@gmail.com>
2022-04-13 15:33:28 +0200

    working on toplevel BM

--- Box_Muller/VHDL/Box_Muller.vhd
+++ Box_Muller/VHDL/Box_Muller.vhd
@@ -1,18 +1,50 @@
--- Leading Ones Detector
 Library ieee; 
 use ieee.std_logic_1164.all;  
 use ieee.std_logic_unsigned.all;
 
 entity Box_Muller is 
-	port (
-		X: in std_logic_vector(1 downto 0);
-		sum: out std_logic_vector(1 downto 0)
+port(
+	gclk : in std_logic;
+	greset : in std_logic;
+	
+	U1 : out std_logic_vector(15 downto 0);
+	U2 : out std_logic_vector(15 downto 0)
 	);
-end Box_Muller;
+end sincos;
 
-architecture behavior of Box_Muller is
+architecture behavioural of Box_Muller is 
 
-begin
-	sum(0) <= X(0) and X(1);
-	sum(1) <= X(0) or X(1);
-end behavior;
+component LFSR_16 is
+	 port (
+		CLK: in std_logic;
+		OUTPUT: out std_logic_vector(15 downto 0)
+	);
+end LFSR_16;
+
+component LFSR_33 is
+	 port (
+		CLK: in std_logic;
+		OUTPUT: out std_logic_vector(32 downto 0)
+	);
+end LFSR_33;
+
+component f_block is 
+	port (
+		U: in std_logic_vector(31 downto 0);
+		clk_in: in std_logic;
+		f_res: out std_logic_vector(15 downto 0)
+		
+	);
+end f_block;
+
+component sincos is
+	port(
+		i_clock : in std_logic;
+		reset : in std_logic;
+		data : in std_logic_vector(11 downto 0);
+		sinOut : out std_logic_vector(15 downto 0);
+		cosOut : out std_logic_vector(15 downto 0)
+	);
+	end component;
+
+end architecture;
