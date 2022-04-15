776366bae7514eb2f55cc7deced7a5f43e819305
M-Lunten <50273518+M-Lunten@users.noreply.github.com>
2022-04-15 17:36:27 +0200

    TB with data collection
    Testbench for the Box-Muller implementation with data collection. A program was added in the matlab folder to show the distribution of this data.

--- Box_Muller/VHDL/Box_Muller.vhd
+++ Box_Muller/VHDL/Box_Muller.vhd
@@ -1,6 +1,6 @@
 Library ieee; 
-use ieee.std_logic_1164.all;  
-use ieee.std_logic_unsigned.all;
+use ieee.std_logic_1164.all;    
+use ieee.numeric_std.all;
 
 entity Box_Muller is 
 port(
@@ -53,23 +53,25 @@ end component;
 	signal sinMul : std_logic_vector(15 downto 0);
 	signal cosMul : std_logic_vector(15 downto 0);
 	signal fblockMul : std_logic_vector(15 downto 0);
-	signal mulres1 : std_logic_vector(31 downto 0);
-	signal mulres2 : std_logic_vector(31 downto 0);
+	signal dummy1 : std_logic_vector(3 downto 0);
+	signal dummy2 : std_logic;
 	
 	begin
-		LF16 : LFSR_16 port map(gclk,OUTPUT(15 downto 4) => LF1sincos);
-		LF33 : LFSR_33 port map(gclk,OUTPUT(31 downto 0) => LF2fblock);
+		LF16 : LFSR_16 port map(gclk,OUTPUT(15 downto 4) => LF1sincos, OUTPUT(3 downto 0) => dummy1);
+		LF33 : LFSR_33 port map(gclk,OUTPUT(31 downto 0) => LF2fblock, OUTPUT(32) => dummy2);
 		f1 : f_block port map(LF2fblock,gclk,fblockMul);
 		sc : sincos port map(gclk,greset,LF1sincos,sinMul,cosMul);
 		
 		process(gclk) is
+		variable mulres1 : signed(31 downto 0);
+		variable mulres2 : signed(31 downto 0);
 			begin	
 				if rising_edge(gclk) then
-					mulres1 <= fblockMul*sinMul;
-					mulres2 <= fblockMul*cosMul;
+					mulres1 := signed(fblockMul)*signed(sinMul);
+					mulres2 := signed(fblockMul)*signed(cosMul);
 					
-					U1 <= mulres1(30 downto 15);
-					U2 <= mulres2(30 downto 15);
+					U1 <= std_logic_vector(mulres1(30 downto 15));
+					U2 <= std_logic_vector(mulres2(30 downto 15));
 					
 				end if;
 		end process;
