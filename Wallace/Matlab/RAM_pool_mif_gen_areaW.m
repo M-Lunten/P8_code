3c29dcb1d1301c2f30889257bad8f073a957f315
M-Lunten <50273518+M-Lunten@users.noreply.github.com>
2022-05-22 11:44:53 +0200

    Area RAM pool file

---
+++ Wallace/Matlab/RAM_pool_mif_gen_areaW.m
@@ -0,0 +1,50 @@
+% Memory initialization file (MIF) for wallace init pool area optimized
+% coefficients
+clear all
+rng(50)
+n_pool = 2;
+pool_size = 512;
+WordLength = 16;
+
+dataPool = randn(1024, 1);
+dataPool = pool_normalization(dataPool);
+
+[fi, xi] = ksdensity(dataPool);    
+x = [-6:.01:6];
+y = normpdf(x,0,1);
+plot(x, y);
+hold on
+plot(xi, fi);
+hold off
+legend('Ideal Gaussian', 'VHDL simulation');
+grid on
+xlim([-5 5])
+
+% Setting for the MIF
+file_name = 'init_pool_area.mif';
+bit_width = 16;
+fraction_length = 11;
+data = dataPool;
+depth = length(data);
+address_type = 'DEC';
+data_type = 'BIN';
+
+% Writing to the file
+file = fopen(file_name, 'wt');
+fprintf(file, 'WIDTH=');
+fprintf(file, '%i;\n', bit_width);
+fprintf(file, 'DEPTH=');
+fprintf(file, '%i;\n', depth);
+fprintf(file, 'ADDRESS_RADIX=');
+fprintf(file, '%s;\n', address_type);
+fprintf(file, 'DATA_RADIX=');
+fprintf(file, '%s;\n', data_type);
+fprintf(file, '\n');
+fprintf(file, 'CONTENT BEGIN\n');
+
+for i = 0:length(data)-1
+    line = bin(sfi(data(i+1), bit_width, fraction_length));
+    fprintf(file, '\t%i : %s;\n', i, line);
+end
+fprintf(file, 'END;');
+fclose(file);
